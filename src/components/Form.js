1cb650756bbbaa62af15f1b06a3b16b613ca3b56
klmeir <caalmeir@gmail.com>
2020-06-05 16:17:12 -0500

    add state to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+
+    const [search, setSearch] = useState({
+        city: '',
+        country: ''
+    });
+
+    const { city, country } = search;
+
+    const handleChange = e => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return ( 
         <form>
             <div className="input-field col s12">
@@ -8,6 +23,8 @@ const Form = () => {
                     type="text"
                     name="city"
                     id="city"
+                    value={city}
+                    onChange={handleChange}
                 />
                 <label htmlFor="city">Ciudad: </label>
             </div>
@@ -16,8 +33,17 @@ const Form = () => {
                 <select
                     name="country"
                     id="country"
+                    value={country}
+                    onChange={handleChange}
                 >
                     <option value="">-- Seleccione un país --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="country">País: </label>
             </div>
