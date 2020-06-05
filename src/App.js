d06d602e1f357ea2c6fa7b9837adeee442ba83f7
klmeir <caalmeir@gmail.com>
2020-06-05 18:21:06 -0500

    sending the request to the api

--- src/App.js
+++ src/App.js
@@ -12,9 +12,18 @@ function App() {
 
   const { city, country } = search;
 
-  useEffect(() => {    
-    const getDataAPI = async () => {
+  useEffect(() => {          
+    const getDataAPI = async () => {      
+      if(consult) {
+        console.log("call getDataAPI");
+        const appId = '5fe09f6f04c1ce67bd933fc70eac82b3';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${appId}`;
 
+        const response = await fetch(url);
+        const result = await response.json();
+
+        console.log(result);
+      }        
     }
     getDataAPI();
   }, [consult])
